7a15a3f1e045a394edc7ba921981f8884f755d3d
Tarun Daharwal <tarun@pratilipi.com>
2025-08-29 06:16:29 +0000

    aapne nifty 50 aur market indicator screen ko bda kr diya hai. 50 pratis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,27 @@ import { TradingTerminal } from '@/components/trading-terminal';
 
 export default function DashboardPage() {
   return (
-    <main className="flex flex-col p-4 md:p-6 space-y-4 md:space-y-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
-        {/* Top-Left */}
-        <div className="flex flex-col">
-          <TradingTerminal />
+    <main className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+      <div className="grid h-[calc(100vh-8rem)] grid-cols-1 gap-4 md:grid-cols-2">
+        {/* Column 1 */}
+        <div className="flex flex-col gap-4">
+          <div className="flex-grow-0 flex-shrink-0 basis-1/2">
+            <TradingTerminal />
+          </div>
+          <div className="flex-1">
+            <OptionChain />
+          </div>
         </div>
-        {/* Top-Right */}
-        <div className="flex flex-col">
-          <IndicatorCards />
-        </div>
-        {/* Bottom-Left */}
-        <div className="flex flex-col">
-          <OptionChain />
-        </div>
-        {/* Bottom-Right */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
-          <PositionsTable />
-          <OrdersTable />
+
+        {/* Column 2 */}
+        <div className="flex flex-col gap-4">
+          <div className="flex-grow-0 flex-shrink-0 basis-1/2">
+            <IndicatorCards />
+          </div>
+          <div className="grid flex-1 grid-cols-1 gap-4 md:grid-cols-2">
+            <PositionsTable />
+            <OrdersTable />
+          </div>
         </div>
       </div>
     </main>

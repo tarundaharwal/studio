73e4cded0419e6ef7ae6e86681b5744079d18850
Tarun Daharwal <tarun@pratilipi.com>
2025-08-29 06:18:40 +0000

    sabse pahle to pahli screen nifty 50 ka screen 50 % vetically kam kar di

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { TradingTerminal } from '@/components/trading-terminal';
 export default function DashboardPage() {
   return (
     <main className="flex-1 space-y-4 p-4 pt-6 md:p-8">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 h-[calc(100vh-8rem)]">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
         {/* Column 1 */}
         <div className="flex flex-col gap-4">
           <div className="flex-[0.5]">
@@ -21,7 +21,7 @@ export default function DashboardPage() {
 
         {/* Column 2 */}
         <div className="flex flex-col gap-4">
-          <div className="flex-[0.5]">
+          <div className="flex-1">
             <IndicatorCards />
           </div>
           <div className="flex-1 grid grid-cols-1 gap-4 md:grid-cols-2">

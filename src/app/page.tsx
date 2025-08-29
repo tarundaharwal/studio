4e198cebc79b4276d9de14d9071c2c482f58ba3b
Tarun Daharwal <tarun@pratilipi.com>
2025-08-29 06:02:25 +0000

    mujhe btao ki kya aap pahli screen yani nifty 50 ko 30 pratishat chota k

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,25 @@ import { TradingTerminal } from '@/components/trading-terminal';
 export default function DashboardPage() {
   return (
     <main className="flex-1 overflow-auto p-4 md:p-6">
-      <div className="grid h-[calc(100vh-8rem)] grid-cols-1 gap-4 md:grid-cols-2 md:gap-6">
-        {/* Top-Left */}
-        <div className="col-span-1 flex flex-col">
-          <TradingTerminal />
+      <div className="grid h-[calc(100vh-8rem)] grid-cols-1 gap-4 md:grid-cols-3 md:gap-6">
+        {/* Left (smaller) */}
+        <div className="col-span-1 flex flex-col gap-4 md:gap-6">
+          <div className="flex-1">
+            <TradingTerminal />
+          </div>
+          <div className="flex-1">
+            <OptionChain />
+          </div>
         </div>
-        {/* Top-Right */}
-        <div className="col-span-1 flex flex-col">
-          <IndicatorCards />
-        </div>
-        {/* Bottom-Left */}
-        <div className="col-span-1 flex flex-col">
-          <OptionChain />
-        </div>
-        {/* Bottom-Right */}
-        <div className="col-span-1 grid grid-rows-2 gap-4 md:gap-6">
-            <PositionsTable />
-            <OrdersTable />
+        {/* Right (larger) */}
+        <div className="col-span-2 flex flex-col gap-4 md:gap-6">
+            <div className="flex-1">
+                <IndicatorCards />
+            </div>
+            <div className="grid grid-cols-2 gap-4 md:gap-6 flex-1">
+                <PositionsTable />
+                <OrdersTable />
+            </div>
         </div>
       </div>
     </main>

f1ed07632132fd8771169e24008759330e3a4b57
Tarun Daharwal <tarun@pratilipi.com>
2025-09-01 10:18:55 +0000

    ab aapko ek kaam krna hai ki nifty 50 ke screen ke pure box size ko 30 p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function DashboardPage() {
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
           {/* Main Column */}
           <div className="col-span-1 flex flex-col gap-6 lg:col-span-2">
-              <div className="h-[500px]">
+              <div className="h-[350px]">
                 <TradingTerminal />
               </div>
               <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
